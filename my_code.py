0d14b4e67f11d8f4a7f0d2896b1aa1f24ebb4070
Felipe Paulino <fpaulinojr@Felipes-MacBook-Air.local>
2024-01-09 15:01:03 -0500

    my first commit

---
+++ my_code.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("What is your name?")
+    print("We want to know if you like programing!")
+    print()
+    answer = input("Do you like programing " + name + "?")
+    print("Great! You said" + answer + "?")
+    print("Let's start learning some Python today")
+
+ if __name__ == "__main__":
+    main()
---
+++ your_code.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("What is your name?")
+    print("We want to know if you like programing!")
+    answer = input("Do you like programing " +name+ "?")
+    print("Great! You said" +answer+ "!")
+    print("Let's start learning some Python today")
+
+if __name__ == "__main__":
+     main()
